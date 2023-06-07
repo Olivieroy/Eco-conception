31714e12c30fcc9d62e955ff6e161b7b8b731cae
Pierre Pineau <pierrepineau.pro@gmail.com>
2023-06-07 12:40:22 +0200

    Commit Pierre

---
+++ layouts/cta.php
@@ -0,0 +1,26 @@
+<section class="layout">
+    <div class="container">
+        <div class="cta__item">
+            <h2 class="text-primary-300 mb-4">L'ère du numérique responsable</h2>
+            <p class="mb-4">L'ère du numérique a apporté de nouvelles opportunités d'apprentissage, mais également de nouveaux défis en termes de durabilité. C'est pourquoi chez Éco-Formation, nous nous engageons à développer et proposer des formations numériques responsables, qui respectent notre planète tout en garantissant une qualité d'enseignement supérieure.</p>
+            <a href="#" class="btn btn-primary">Cherchez votre formation dès maintenant ! <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right"><line x1="5" x2="19" y1="12" y2="12"/><polyline points="12 5 19 12 12 19"/></svg></a>
+        </div>
+
+        <div class="cta__item">
+            <h2 class="text-primary-300 mb-4">Notre mission</h2>
+            <p class="mb-4">Faire de vous des apprenants du 21e siècle, à l'aise avec les technologies actuelles et conscients de leur impact sur notre environnement. Nous vous offrons une variété de formations en ligne, conçues pour vous aider à acquérir les compétences digitales essentielles tout en minimisant votre empreinte écologique.</p>
+        </div>
+
+        <div class="cta__item">
+            <h2 class="text-primary-300 mb-4">Découvrez nos formations</h2>
+            <p class="mb-4">Nos formations innovantes sont conçues avec un souci constant d'éco-responsabilité. Elles sont réalisées en privilégiant les techniques de numérique durable, visant à réduire l'empreinte carbone liée à l'apprentissage digital.</p>
+            <a href="#" class="btn btn-primary">Trouvez votre formation idéale ici ! <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right"><line x1="5" x2="19" y1="12" y2="12"/><polyline points="12 5 19 12 12 19"/></svg></a>
+        </div>
+
+        <div class="cta__item">
+            <h2 class="text-primary-300 mb-4">Contactez-nous</h2>
+            <p class="mb-4">Avez-vous des questions ou souhaitez-vous obtenir plus d'informations ?</p>
+            <a href="#" class="btn btn-primary">Contactez-nous dès maintenant ! <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right"><line x1="5" x2="19" y1="12" y2="12"/><polyline points="12 5 19 12 12 19"/></svg></a>
+        </div>
+    </div>
+</section>
---
+++ layouts/hero.php
@@ -0,0 +1,5 @@
+<section class="layout hero">
+    <div class="container">
+        <h1>Bienvenue sur <strong class="text-primary-300">Éco-Formation</strong> <br>votre site sur les formations <strong class="text-primary-300">numérique responsable</strong></h1>
+    </div>
+</section>
